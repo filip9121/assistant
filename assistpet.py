4158763233aca193cba930360c37f1cabc5a17b7
Filip <151365055+filip9121@users.noreply.github.com>
2025-01-08 12:30:58 +0100

    Update assistpet.py

--- assistpet.py
+++ assistpet.py
@@ -1,3 +1,16 @@
 import tkinter
 import pygame
-import SpeechRecognition
+import SpeechRecognition as sr #shortcut
+import time
+
+pygame.init()
+
+
+class Assistant: #functions
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Assistant")
+        self.root.geometry("300x400")
+
+  def start_animation(self)
+self.running=true
